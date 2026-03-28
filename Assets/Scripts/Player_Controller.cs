fbac4240958ac387e78dfc7d0173badf6cf45430
robinjamesmusic <rcrinson@hotmail.com>
2026-03-28 20:23:33 +0000

    Added Torque

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -3,11 +3,14 @@ using UnityEngine.InputSystem;
 
 public class Player_Controller : MonoBehaviour
 {
+    [SerializeField] float torqueAmount = 1f;
     InputAction moveAction;
+    Rigidbody2D myRigidbody2D;
 
      void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
+        myRigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     
@@ -16,7 +19,15 @@ public class Player_Controller : MonoBehaviour
         
         Vector2 moveVector;
         moveVector = moveAction.ReadValue<Vector2>();
-        print(moveVector);
+        if (moveVector.x<0)
+
+         myRigidbody2D.AddTorque(torqueAmount);
+
+        else if (moveVector.x>0)
+
+         myRigidbody2D.AddTorque(-torqueAmount);
+
+
 
     }
 }
