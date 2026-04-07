e4c8cc12b303ca0244bc41a68417257b70239dcb
robinjamesmusic <rcrinson@hotmail.com>
2026-04-07 20:34:36 +0100

    AddedBoostSpeed

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -4,20 +4,30 @@ using UnityEngine.InputSystem;
 public class Player_Controller : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float baseSpeed = 20f;
+    [SerializeField] float boostSpeed = 20f;
     InputAction moveAction;
     Rigidbody2D myRigidbody2D;
+    SurfaceEffector2D surfaceEffector2D;
+    Vector2 moveVector;
 
      void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
         myRigidbody2D = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindFirstObjectByType<SurfaceEffector2D>();
     }
 
     
     void Update()
     {
+        RotatePlayer();
+        BoostPlayer();
+    }
+
+    void RotatePlayer()
+    {
         
-        Vector2 moveVector;
         moveVector = moveAction.ReadValue<Vector2>();
         if (moveVector.x<0)
 
@@ -26,8 +36,14 @@ public class Player_Controller : MonoBehaviour
         else if (moveVector.x>0)
 
          myRigidbody2D.AddTorque(-torqueAmount);
-
-
-
+    }
+    
+    void BoostPlayer()
+    {
+        if (moveVector.y > 0)
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        
     }
 }
